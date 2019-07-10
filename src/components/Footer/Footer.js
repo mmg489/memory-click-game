9c75dea0cb8c1b3bb3239c7992686929d4b59c9d
Margarita <m_mg489@outlook.com>
2019-07-09 23:51:15 -0400

    setup footer, exported footer, added image for footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <footer className="footer">
+        <div className="bottom">
+            Memory Click Game! <img alt="stars" src="./assets/images/patStars.png" />
+        </div>
+    </footer>
+);
+
+export default Footer;
