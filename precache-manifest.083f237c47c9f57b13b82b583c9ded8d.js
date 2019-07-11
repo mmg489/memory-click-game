97ea2e558453ca7a074d07514b93b489dc1733c4
Margarita <m_mg489@outlook.com>
2019-07-10 22:34:16 -0400

    Updates

---
+++ precache-manifest.083f237c47c9f57b13b82b583c9ded8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab6b7b869d993b01d6d172d4f4d88cf",
+    "url": "/memory-click-game/index.html"
+  },
+  {
+    "revision": "66b5d273e8a30e9fcbcc",
+    "url": "/memory-click-game/static/css/main.5234653b.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/memory-click-game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "66b5d273e8a30e9fcbcc",
+    "url": "/memory-click-game/static/js/main.a8b38fea.chunk.js"
+  },
+  {
+    "revision": "8219c3139a714514a4a0",
+    "url": "/memory-click-game/static/js/runtime~main.ef35aeed.js"
+  }
+]);
